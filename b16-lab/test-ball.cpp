1014086ebd70b3d553b0a744d70116a010aa4c4d
gabriel1997castro <gabriel1997.castro@gmail.com>
2017-04-24 21:14:54 -0300

    Add the question 6 task 10

--- b16-lab/test-ball.cpp
+++ b16-lab/test-ball.cpp
@@ -4,14 +4,19 @@
  **/
 
 #include "ball.h"
+#include <iostream>
+const double dt = 1.0/30 ;
+
+// Função da questão 6 Task 10
+ void run(Simulation & ball, double dt) {
+   for (int i = 0 ; i < 100 ; ++i) 
+   	{ ball.step(dt) ; ball.display() ; }
+ }
 
 int main(int argc, char** argv)
 {
   Ball ball ;
-  const double dt = 1.0/30 ;
-  for (int i = 0 ; i < 100 ; ++i) {
-    ball.step(dt) ;
-    ball.display() ;
-  }
+
+  run(ball, dt);
   return 0 ;
 }
